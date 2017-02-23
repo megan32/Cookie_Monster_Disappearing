9981d05e49cc932292262d6bddb88d959cee92ac
Megan <meganvieno@gmail.com>
2017-02-23 06:06:34 -0800

    updating docs for copy to personal repo

---
+++ cookieM.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+ $('img').click(function(){
+   $(this).css("visibility", "hidden");
+ });
+
+$('#restore').click(function(){
+  $('img').css("visibility","visible");
+});
+
+});
